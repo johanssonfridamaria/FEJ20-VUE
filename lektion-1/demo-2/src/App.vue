f3f643fa9bdb2c96b7c41091ddc0222dce1a44d3
Isedor021 <joakim.wahlstrom@lexicon.se>
2020-11-16 16:08:17 +0100

    lektion1

--- lektion-1/demo-2/src/App.vue
+++ lektion-1/demo-2/src/App.vue
@@ -12,6 +12,7 @@
 import Navbar from './components/Navbar'
 import Carousel from './components/Carousel'
 import CardDeck from './components/cards/CardDeck'
+
 export default {
   name: 'App',
   components: {
