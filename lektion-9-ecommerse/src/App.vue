b94e71fe64ceeb63fb5e2c5deb59a9d0f7651c43
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2020-12-15 10:00:34 +0100

    lektion-9

---
+++ lektion-9-ecommerse/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <navbar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navigation/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
